ec2c5f3990bdf7f8d48cfe82469bf9fc6440e4ac
Leonel Herrera <113858983+LeoHrr@users.noreply.github.com>
2023-03-12 18:05:35 -0300

    first commit

---
+++ BackEnd/src/main/java/com/portfolio/hla/HlaApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.hla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlaApplication.class, args);
+	}
+
+}
---
+++ BackEnd/src/test/java/com/portfolio/hla/HlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.hla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
