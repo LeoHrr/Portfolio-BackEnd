a6105231419e503b439b0e1cef69391bd9ba5390
Leonel Herrera <113858983+LeoHrr@users.noreply.github.com>
2023-03-13 22:13:06 -0300

    Update PersonaController.java

--- BackEnd/hla/src/main/java/com/portfolioap/hla/Controller/PersonaController.java
+++ BackEnd/hla/src/main/java/com/portfolioap/hla/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolioap.hla.Entity.Persona;
 import com.portfolioap.hla.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,10 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Leo
  */
 @RestController
+@CrossOrigin(origins = "Http://localhost:4200")
+
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
@@ -50,5 +53,9 @@ public class PersonaController {
         
         ipersonaService.savePersona(persona);
         return persona;
+    }
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
     }
 }
